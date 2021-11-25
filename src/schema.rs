74a787098a15ce12d2730e3d2b66c6ae0ab78e66
lucca <rkaasgaard@gmail.com>
2021-11-25 18:34:13 +0100

    feat(schema): networks can be named and empty devices can be added

--- src/schema.rs
+++ src/schema.rs
@@ -3,6 +3,7 @@ use uuid::Uuid;
 
 #[derive(Serialize, Deserialize)]
 pub struct Schema {
+    pub name: String,
     pub meta: Meta,
     pub device: Vec<Device>,
 }
@@ -15,6 +16,7 @@ pub struct Meta {
 impl Schema {
     pub fn new(id: Uuid) -> Schema {
         Schema {
+            name: "".to_owned(),
             meta: Meta { id },
             device: vec![],
         }
@@ -24,18 +26,46 @@ impl Schema {
 #[derive(Serialize, Deserialize)]
 pub struct Device;
 
+impl Device {
+    pub fn new() -> Self {
+        Device {}
+    }
+}
+
+impl Default for Device {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 pub struct SchemaBuilder {
+    name: String,
     id: Uuid,
     device: Vec<Device>,
 }
 
 impl SchemaBuilder {
-    pub fn new(id: Uuid) -> SchemaBuilder {
-        SchemaBuilder { id, device: vec![] }
+    pub fn new(id: Uuid) -> Self {
+        SchemaBuilder {
+            name: "".to_owned(),
+            id,
+            device: vec![],
+        }
+    }
+
+    pub fn named(mut self, name: String) -> Self {
+        self.name = name;
+        self
+    }
+
+    pub fn add_device(mut self, device: Device) -> Self {
+        self.device.push(device);
+        self
     }
 
     pub fn create(self) -> Schema {
         Schema {
+            name: self.name,
             meta: Meta { id: self.id },
             device: self.device,
         }
--- src/schema_test.rs
+++ src/schema_test.rs
@@ -9,4 +9,18 @@ mod schema_builder_test {
         let schema = SchemaBuilder::new(id).create();
         assert!(schema.device.is_empty())
     }
+
+    #[test]
+    fn can_name_the_network() {
+        let id = Uuid::new_v4();
+        let schema = SchemaBuilder::new(id).named("test".to_owned()).create();
+        assert_eq!("test", schema.name)
+    }
+
+    #[test]
+    fn can_add_device_to_network() {
+        let id = Uuid::new_v4();
+        let schema = SchemaBuilder::new(id).add_device(Device::new()).create();
+        assert!(!schema.device.is_empty())
+    }
 }
