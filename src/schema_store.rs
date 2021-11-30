67b86bffbd885f1905d7b85869331c5b11507a04
lucca <rkaasgaard@gmail.com>
2021-11-30 10:43:24 +0100

    feat(schema): data store propagates errors

--- src/schema_store.rs
+++ src/schema_store.rs
@@ -1,4 +1,5 @@
 use serde_json;
+use std::error::Error;
 use std::fs::{read_to_string, DirBuilder, File};
 use uuid::Uuid;
 
@@ -17,17 +18,14 @@ pub fn save(schema: Schema) {
     )
     .unwrap();
 }
-pub fn load(id: Uuid) -> Result<Schema, LoadError> {
+pub fn load(id: Uuid) -> Result<Schema, Box<dyn Error>> {
     let contents = match read_to_string("network_instance/".to_owned() + &id.to_string() + ".json")
     {
         Ok(s) => s,
-        Err(_) => return Err(LoadError),
+        Err(e) => return Err(Box::new(e)),
     };
     match serde_json::from_str(&contents) {
         Ok(s) => Ok(s),
-        Err(_) => Err(LoadError),
+        Err(e) => Err(Box::new(e)),
     }
 }
-
-#[derive(Debug)]
-pub struct LoadError;
